6ff85b5e625d74a9d613ef111054a3db40525651
Xeuz-x <grey69528@gmail.com>
2021-01-26 18:55:25 +1300

    Removed default values

--- Util_Embed.py
+++ Util_Embed.py
@@ -10,22 +10,31 @@ async def timeOutError(date, channel):
     Args:
         date (STR): Name of card
         channel (OBJ): Discord channel object
+        
+    Example:
+    
+          Statistics
+    ----------------------
+    Runtime(Ave):: 53.5s
+    Iterations:: 2047
+    Raised Errors:: 1
+
+    API Usage: 22.2%
+    ----------------------
+    [####----------------]
     """
                  
     embed = discord.Embed(title="🚨 Timeout Error 🚨", colour=discord.Colour(0x12e4ff), url="https://discordapp.com")
-    embed.set_image(url="https://banner2.cleanpng.com/20181115/fb/kisspng-illustration-image-dogeza-apologia-blog-5bee40a206d977.4177758715423407700281.jpg")
-    embed.set_thumbnail(url="https://uxwing.com/wp-content/themes/uxwing/download/07-design-and-development/sorry.png")
-    embed.set_author(name="Xeus", url="https://twitter.com/xKnight_Grey", icon_url="https://cdn.discordapp.com/app-icons/795966880779206656/8b6d6c8e089b7cd499435333dd3c0bf3.png?size=64")
-    embed.set_footer(text="Do people even read this?", icon_url="https://pbs.twimg.com/profile_images/1334111535082446849/LdHGWkZq_400x400.jpg")
-    embed.add_field(name=f"Mail Sent: Timeout Error {date}", value=f"Sorry for the inconvenience.", inline = False)
+    embed.set_image(url="")
+    embed.set_thumbnail(url="")
+    embed.set_author(name="Name", url="", icon_url="")
+    embed.set_footer(text="", icon_url="")
+    embed.add_field(name=f"", value=f"", inline = False)
     await channel.send(embed=embed)
     
 
     ratio = (config.totalCallCount)/(config.executionTime * 30)
-    percent = round(ratio * 100 , 1)
-    loaded = round(ratio * 20) * "#"
-    unloaded = (20 - round(ratio * 20)) * "-"
-    progressBar = '[' + loaded + unloaded + ']'
+    progressBar = '[' + (round(ratio * 20) * "#") + ((20 - round(ratio * 20)) * "-") + ']'
     string = f"```asciidoc\n      Statistics\n----------------------\nRuntime(Ave):: {config.executionTime:.1f}s\nItererations:: {config.iterationCounter}\nRaised Errors:: {config.nErrors}\n   API Usage: {percent}%\n----------------------\n{progressBar}```"
     await channel.send(string)
     
@@ -36,34 +45,38 @@ async def performanceChart(channel = config.channelID_M):
 
     Args:
         channel (OBJ, optional): Discord object, for deciding channel location. Defaults to config.channelID_M.
+        
+        
+    Example:
+    
+     Board: Writing (184)
+    ----------------------
+    Open:: 136
+    In Progress:: 28
+    Done:: 20
+    Total Progress: 10.87%
+    ----------------------
+    [##------------------]
     """
     embed = discord.Embed(title="Daily Management Report", colour=discord.Colour(0x3b91ff), url="https://discordapp.com", description=f"```asciidoc\n Date: {datetime.now().strftime('%m/%d/%Y')}\n------------------```")
-    embed.set_image(url="https://media-exp1.licdn.com/dms/image/C4E1BAQFYaGosTkluEQ/company-background_10000/0/1564129197188?e=2159024400&v=beta&t=6itWEitI2AFnEOPK9kU4FgN2U2a8yUqLkiSH1NJ1xms")
-    embed.set_thumbnail(url="https://pbs.twimg.com/profile_images/1334111535082446849/LdHGWkZq_400x400.jpg")
-    embed.set_footer(text="We can do this! 👊", icon_url="https://pbs.twimg.com/profile_images/1334111535082446849/LdHGWkZq_400x400.jpg")
+    embed.set_image(url="")
+    embed.set_thumbnail(url="")
+    embed.set_footer(text="We can do this! 👊", icon_url="")
     await channel.send(embed=embed)
     
     ratio = (config.countW_Done)/(config.countW_P + config.countW_TODO + config.countW_Done)
-    percent = round(ratio * 100 , 2)
-    loaded = round(ratio * 20) * "#"
-    unloaded = (20 - round(ratio * 20)) * "-"
-    progressBar = '[' + loaded + unloaded + ']'
+    progressBar = '[' + (round(ratio * 20) * "#") + ((20 - round(ratio * 20)) * "-") + ']'
     string = f"```asciidoc\nBoard: Writing ({config.countW})\n----------------------\nOpen:: {config.countW_TODO}\nIn Progress:: {config.countW_P}\nDone:: {config.countW_Done}\nTotal Progress: {percent}%\n----------------------\n{progressBar}```"
     await channel.send(string)
 
     ratio = (config.countA_Done)/(config.countA_P + config.countA_TODO + config.countA_Done)
     percent = round(ratio * 100 , 2)
-    loaded = round(ratio * 20) * "#"
-    unloaded = (20 - round(ratio * 20)) * "-"
-    progressBar = '[' + loaded + unloaded + ']'
+    progressBar = '[' + (round(ratio * 20) * "#") + ((20 - round(ratio * 20)) * "-") + ']'
     string = f"```asciidoc\nBoard: Art ({config.countA})\n----------------------\nOpen:: {config.countA_TODO}\nIn Progress:: {config.countA_P}\nDone:: {config.countA_Done}\nTotal Progress: {percent}%\n----------------------\n{progressBar}```"
     await channel.send(string)
     
     ratio = (config.countS_Done)/(config.countS_P + config.countS_TODO + config.countS_Done)
-    percent = round(ratio * 100 , 2)
-    loaded = round(ratio * 20) * "#"
-    unloaded = (20 - round(ratio * 20)) * "-"
-    progressBar = '[' + loaded + unloaded + ']'
+    progressBar = '[' + (round(ratio * 20) * "#") + ((20 - round(ratio * 20)) * "-") + ']'
     string = f"```asciidoc\nBoard: Sound ({config.countS})\n----------------------\nOpen:: {config.countS_TODO}\nIn Progress:: {config.countS_P}\nDone:: {config.countS_Done}\nTotal Progress: {percent}%\n----------------------\n{progressBar}```"
     await channel.send(string)
     
@@ -78,12 +91,12 @@ async def cardCreated(cardName, channel):
     """
     if (config.lastAction != f"Card Created {cardName}"):
         embed = discord.Embed(title="🚨 WEE WOO WEE WOO 🚨", colour=discord.Colour(0x12e4ff), url="https://discordapp.com", description="```asciidoc\n[A NEW TODO HAS BEEN CREATED!]```")
-        embed.set_image(url="https://thewhisp.mommyish.com/wp-content/uploads/2018/12/Spongebob-Mailbox.jpg")
-        embed.set_thumbnail(url="https://simpleskincarescience.com/wp-content/uploads/2019/11/smiling-spongebob-fish-e1574132873854.jpg")
-        embed.set_author(name="Postman Phat", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/app-icons/795966880779206656/8b6d6c8e089b7cd499435333dd3c0bf3.png?size=64")
-        embed.set_footer(text="Do people even read this?", icon_url="https://pbs.twimg.com/profile_images/1334111535082446849/LdHGWkZq_400x400.jpg")
-        embed.add_field(name=f"New ToDo - {cardName}", value="Visit trello for more information!", inline = False)
-        config.lastAction = f"Card Created {cardName}"
+        embed.set_image(url="")
+        embed.set_thumbnail(url="")
+        embed.set_author(name="", url="")
+        embed.set_footer(text="", icon_url="")
+        embed.add_field(name=f"", value="", inline = False)
+        config.lastAction = f""
         await channel.send(embed=embed)
 
 
@@ -99,11 +112,11 @@ async def cardMovedUp(cardName, destination, channel, author):
     """
     if (config.lastAction != f"Card MovedUP {cardName}"):
         embed = discord.Embed(title="🚨 WEE WOO WEE WOO 🚨", colour=discord.Colour(0x12e4ff), url="https://discordapp.com", description="```css\nWe need more people like you!\nYou're a star!💪🌟```")
-        embed.set_image(url="https://i.ytimg.com/vi/oZw26clV1UI/maxresdefault.jpg")
-        embed.set_thumbnail(url="https://fhr.fra1.cdn.digitaloceanspaces.com/NHLGamer/ECL/ECL_8/team_logos/Pro/ECL8%20Pro%20-%20Poggers.png")
-        embed.set_author(name="Postman Phat", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/app-icons/795966880779206656/8b6d6c8e089b7cd499435333dd3c0bf3.png?size=64")
-        embed.set_footer(text="Do people even read this?", icon_url="https://pbs.twimg.com/profile_images/1334111535082446849/LdHGWkZq_400x400.jpg")
-        embed.add_field(name=f"{cardName} has advanced to {destination}!!!", value=f"Made by: {author}", inline = False)
+        embed.set_image(url="")
+        embed.set_thumbnail(url="")
+        embed.set_author(name="", url="", icon_url="")
+        embed.set_footer(text="", icon_url="")
+        embed.add_field(name=f"", value=f"", inline = False)
         config.lastAction = f"Card MovedUP {cardName}"
         await channel.send(embed=embed)
         
@@ -120,11 +133,11 @@ async def cardOnHold(cardName, channel, author):
     """
     if (config.lastAction != f"Card OnHold {cardName}"):
         embed = discord.Embed(title="🚨 WEE WOO WEE WOO 🚨", colour=discord.Colour(0x12e4ff), url="https://discordapp.com", description="```css\n😥😥😥😥😥```")
-        embed.set_image(url="https://i.imgflip.com/1o12mo.jpg")
-        embed.set_thumbnail(url="https://img2.wikia.nocookie.net/__cb20130524235901/fakemon/es/images/3/39/Crying_Meme.png")
-        embed.set_author(name="Postman Phat", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/app-icons/795966880779206656/8b6d6c8e089b7cd499435333dd3c0bf3.png?size=64")
-        embed.set_footer(text="Do people even read this?", icon_url="https://pbs.twimg.com/profile_images/1334111535082446849/LdHGWkZq_400x400.jpg")
-        embed.add_field(name=f"{cardName} has been put on hold!", value=f"Made by: {author}", inline = False)
+        embed.set_image(url="")
+        embed.set_thumbnail(url="")
+        embed.set_author(name="", url="", icon_url="")
+        embed.set_footer(text="", icon_url="")
+        embed.add_field(name=f"", value=f"", inline = False)
         config.lastAction = f"Card OnHold {cardName}"
         await channel.send(embed=embed)
         
@@ -141,11 +154,11 @@ async def cardSkippedUp(cardName, source, destination, channel, author):
     """ 
     if (config.lastAction != f"Card SkippedUP {cardName}"):
         embed = discord.Embed(title="🚨 WEE WOO WEE WOO 🚨", colour=discord.Colour(0x12e4ff), url="https://discordapp.com", description="```css\nIt skipped a few steps.....\nWe need more people like you!\nYou're a star!💪🌟```")
-        embed.set_image(url="https://i.ytimg.com/vi/oZw26clV1UI/maxresdefault.jpg")
-        embed.set_thumbnail(url="https://fhr.fra1.cdn.digitaloceanspaces.com/NHLGamer/ECL/ECL_8/team_logos/Pro/ECL8%20Pro%20-%20Poggers.png")
-        embed.set_author(name="Postman Phat", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/app-icons/795966880779206656/8b6d6c8e089b7cd499435333dd3c0bf3.png?size=64")
-        embed.set_footer(text="Do people even read this?", icon_url="https://pbs.twimg.com/profile_images/1334111535082446849/LdHGWkZq_400x400.jpg")
-        embed.add_field(name=f"{cardName} has advanced to {destination}!!!", value=f"Made by: {author}", inline = False)
+        embed.set_image(url="")
+        embed.set_thumbnail(url="")
+        embed.set_author(name="", url="", icon_url="")
+        embed.set_footer(text="", icon_url="")
+        embed.add_field(name=f"", value=f"", inline = False)
         config.lastAction = f"Card SkippedUP {cardName}"
         await channel.send(embed=embed)
 
@@ -162,10 +175,10 @@ async def cardErraticMove(cardName, source, destination, channel, author):
     """
     if (config.lastAction != f"Card ErraticMove {cardName}"):
         embed = discord.Embed(title="🚨 Card Moved Erratically..EHH?! 🚨", colour=discord.Colour(0x12e4ff), url="https://discordapp.com", description="```asciidoc\n[That wasn't suppose to happen...]```")
-        embed.set_image(url="https://www.centralfloridapost.com/wp-content/uploads/2020/06/flat1000x1000075f.u1.jpg")
-        embed.set_thumbnail(url="https://external-preview.redd.it/u08WRG2L8S9j8BDgmn9kHTQy4bqN1v_dj3KlUiYDJKU.jpg?auto=webp&s=860bd654a0b30fa4582ea84cc5087ed7d3286928")
-        embed.set_author(name="Postman Phat", url="https://discordapp.com", icon_url="https://cdn.discordapp.com/app-icons/795966880779206656/8b6d6c8e089b7cd499435333dd3c0bf3.png?size=64")
-        embed.set_footer(text="Do people even read this?", icon_url="https://pbs.twimg.com/profile_images/1334111535082446849/LdHGWkZq_400x400.jpg")
-        embed.add_field(name=f"Card moved from '{source}' to '{destination}'", value=f"{cardName} - {author}", inline = False)
-        config.lastAction = f"Card ErraticMove {cardName}"
+        embed.set_image(url="")
+        embed.set_thumbnail(url="")
+        embed.set_author(name="", url="", icon_url="")
+        embed.set_footer(text="", icon_url="")
+        embed.add_field(name=f"", inline = False)
+        config.lastAction = f""
         await channel.send(embed=embed)
